e521840c0d037f109bbfe8281e42baa0e58ca3e3
hasanjafri <hasan.jafri7@gmail.com>
2019-11-20 00:26:35 -0500

    WIP: Ask a question

--- src/app/components/ask-question/ask-question.component.ts
+++ src/app/components/ask-question/ask-question.component.ts
@@ -17,15 +17,49 @@ export class AskQuestionComponent implements OnInit {
   departments = ['Engineering', 'Management', 'Business', 'Admin', 'Security'];
   locations = ['Canada', 'USA', 'Singapore', 'London', 'India', 'Dubai', 'Japan', 'China'];
   events = [];
+  tempData = [];
+  private eventsSubject: Subject<any>;
 
   enteredUserId: string;
   verifiedUserId: string;
+  verifiedUserName: string;
+  verifiedDepartment: string;
+  verifiedLocation: string;
   verifyUserSubject: Subject<any>;
   askedQuestion: string;
+  selectedEvent: number;
 
   constructor(private eventsService: EventsService, @Inject(OVERLAY_DATA) public overlayProps) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log(this.overlayProps);
+    this.eventsSubject = this.eventsService.pullEvents();
+    this.eventsSubject.subscribe((eventData) => {
+      const eventsData = JSON.parse(eventData.data);
+      if (!eventsData.hasOwnProperty('msgId')) {
+        eventsData.forEach((event) => {
+          if (this.checkNonDuplicateEvent(event)) {
+            this.events.push(event);
+          }
+        });
+        if (this.overlayProps.eventId) {
+          this.selectedEvent = this.events.findIndex((event) => event.eventId === this.overlayProps.eventId);
+        }
+      }
+    });
+    setTimeout(() => {
+      this.eventsSubject.next('pullEvent');
+    }, 2000);
+  }
+
+  checkNonDuplicateEvent(event) {
+    this.events.forEach((existingEvent) => {
+      if (existingEvent.eventId === event.eventId) {
+        return false;
+      }
+    });
+    return true;
+  }
 
   searchUser(evt, userId) {
     evt.preventDefault();
@@ -35,6 +69,12 @@ export class AskQuestionComponent implements OnInit {
       this.verifyUserSubject.subscribe((resp) => {
         const user = JSON.parse(resp.data);
         console.log(user);
+        if (user.id !== null) {
+          this.verifiedUserId = user.id;
+          this.verifiedUserName = user.name;
+          this.verifiedDepartment = user.department;
+          this.verifiedLocation = user.siteLocation;
+        }
       });
       setTimeout(() => {
         this.verifyUserSubject.next('pullEvent');
